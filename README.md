de506c743a3ae8de6b3437d9a063c9ab1b2ad0f8
Song <49385128+songsongsun@users.noreply.github.com>
2022-02-23 11:15:22 +0800

    初始化工程

---
+++ README.md
@@ -0,0 +1,3 @@
+# MySpringBoot
+
+demo
---
+++ src/main/java/com/learn/song/myspringboot/MySpringBootApplication.java
@@ -0,0 +1,16 @@
+package com.learn.song.myspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Song
+ */
+@SpringBootApplication
+public class MySpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringBootApplication.class, args);
+    }
+
+}
